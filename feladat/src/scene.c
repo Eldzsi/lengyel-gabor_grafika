35605d0ec4d638defbef347e93ace90dcb67b6b2
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-03-06 18:19:12 +0100

    Remove unneunnecessary code

--- feladat/src/scene.c
+++ feladat/src/scene.c
@@ -73,25 +73,5 @@ void update_scene(Scene* scene) {
 void render_scene(const Scene* scene) {
     set_material(&(scene->material));
     set_lighting();
-    draw_origin();
     draw_model(&(scene->cube));
-}
-
-
-void draw_origin() {
-    glBegin(GL_LINES);
-
-    glColor3f(1, 0, 0);
-    glVertex3f(0, 0, 0);
-    glVertex3f(1, 0, 0);
-
-    glColor3f(0, 1, 0);
-    glVertex3f(0, 0, 0);
-    glVertex3f(0, 1, 0);
-
-    glColor3f(0, 0, 1);
-    glVertex3f(0, 0, 0);
-    glVertex3f(0, 0, 1);
-
-    glEnd();
 }
