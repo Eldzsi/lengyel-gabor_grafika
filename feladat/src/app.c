23533bad63196eea2f34020a59c9d93744f96465
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-03-06 18:16:48 +0100

    Rename app

--- feladat/src/app.c
+++ feladat/src/app.c
@@ -17,7 +17,7 @@ void init_app(App* app, int width, int height) {
     }
 
     app->window = SDL_CreateWindow(
-        "Cube!",
+        "Game",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         width, height,
         SDL_WINDOW_OPENGL);
