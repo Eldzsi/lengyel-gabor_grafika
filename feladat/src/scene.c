9a69f3a8e311c6f5ce4bbb639ce5890c4ef14974
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-03-07 11:24:32 +0100

    Modify lighting

--- feladat/src/scene.c
+++ feladat/src/scene.c
@@ -6,31 +6,31 @@
 
 void init_scene(Scene* scene) {
     load_model(&(scene->grass), "assets/models/grass.obj");
-    //scene->texture_id = load_texture("assets/textures/grass.jpg");
+    scene->texture_id = load_texture("assets/textures/grass.jpg");
 
     glBindTexture(GL_TEXTURE_2D, scene->texture_id);
 
-    scene->material.ambient.red = 0.0;
-    scene->material.ambient.green = 0.0;
-    scene->material.ambient.blue = 0.0;
+    scene->material.ambient.red = 1.0;
+    scene->material.ambient.green = 1.0;
+    scene->material.ambient.blue = 1.0;
 
     scene->material.diffuse.red = 1.0;
-    scene->material.diffuse.green = 1.0;
-    scene->material.diffuse.blue = 0.0;
+    scene->material.diffuse.green = 0.5;
+    scene->material.diffuse.blue = 1.0;
 
-    scene->material.specular.red = 0.0;
-    scene->material.specular.green = 0.0;
-    scene->material.specular.blue = 0.0;
+    scene->material.specular.red = 1.0;
+    scene->material.specular.green = 1.0;
+    scene->material.specular.blue = 1.0;
 
     scene->material.shininess = 0.0;
 }
 
 
 void set_lighting() {
-    float ambient_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    float diffuse_light[] = { 1.0f, 1.0f, 1.0, 1.0f };
-    float specular_light[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    float position[] = { 0.0f, 0.0f, 10.0f, 1.0f };
+    float ambient_light[] = { 0.3f, 0.2f, 0.1f, 1.0f };
+    float diffuse_light[] = { 0.0f, 0.3f, 0.3f, 1.0f };
+    float specular_light[] = { 0.3f, 0.2f, 0.1f, 1.0f };
+    float position[] = { 0.0f, 0.0f, 3.0f, 1.0f };
 
     glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light);
